7dc3dd10a0f1effaccbd36f72d80048a60e6d5e8
rumur <rumur.dev@gmail.com>
2024-11-22 19:17:21 +0100

    New: Introduce HTTP Client to resolve WordPress requests concurrently

---
+++ src/Support/Client/FailToResolve.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rumur\WordPress\CosmoUsers\Support\Dispatcher;
+
+use Rumur\WordPress\CosmoUsers\Exception;
+
+/**
+ * Class FailToResolve.
+ *
+ * @since 0.1.0
+ *
+ * @package Rumur\WordPress\CosmoUsers\Support\Client
+ */
+class FailToResolve extends Exception
+{
+}
