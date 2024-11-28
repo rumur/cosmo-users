4e2ea912b6a36709e79a67e82a92ea49fa74c01b
rumur <rumur.dev@gmail.com>
2024-11-28 13:21:09 +0100

    Chore: enhance Collection instance

--- src/Collection.php
+++ src/Collection.php
@@ -10,11 +10,13 @@ namespace Rumur\WordPress\CosmoUsers;
  * @template TKey
  * @template TValue
  *
+ * @internal
+ *
  * @since 0.1.0
  *
  * @package Rumur\WordPress\CosmoUsers
  */
-class Collection implements Arrayable, \IteratorAggregate, \Countable
+class Collection implements Arrayable, \IteratorAggregate, \Countable, \JsonSerializable
 {
     /**
      * @param array<TKey,TValue> $items The array of items.
@@ -44,6 +46,52 @@ class Collection implements Arrayable, \IteratorAggregate, \Countable
     }
 
     /**
+     * Maps the items into a new collection.
+     *
+     * @param callable(TValue,TKey):TValue $callback The callback to filter the items.
+     *
+     * @return static<TKey,TValue> The new instance of the collection.
+     */
+    public function map(callable $callback): static
+    {
+        $result = [];
+
+        foreach ($this->items as $key => $item) {
+            $result[$key] = $callback($item, $key);
+        }
+
+        return new static($result);
+    }
+
+    /**
+     * Filters the items in the collection by a given callback or keys.
+     *
+     * @param callable(TValue,TKey):bool|array $keys The list of keys to exclude or a callback.
+     *
+     * @return static<TKey,TValue> The new instance of the collection.
+     */
+    public function except(callable|array $keys): static
+    {
+        if (is_callable($keys)) {
+            $keys = array_filter($this->items, $keys, ARRAY_FILTER_USE_BOTH);
+        }
+
+        return new static(
+            array_diff_key($this->items, array_flip($keys))
+        );
+    }
+
+    /**
+     * Get all the items in the collection.
+     *
+     * @return array<TKey, TValue>
+     */
+    public function all(): array
+    {
+        return $this->items;
+    }
+
+    /**
      * Returns the array of items.
      *
      * @return array<TKey,TValue> The array of items.
@@ -68,5 +116,15 @@ class Collection implements Arrayable, \IteratorAggregate, \Countable
     public function getIterator(): \Traversable
     {
         return new \ArrayIterator($this->items);
+    }
+
+    /**
+     * Serializes the collection to JSON.
+     *
+     * @return array<TKey,TValue> The array of items.
+     */
+    public function jsonSerialize(): mixed
+    {
+        return $this->toArray();
     }
 }
